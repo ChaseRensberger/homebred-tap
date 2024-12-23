31d126b37a273bde52b61678e65c772d9a7cd3fb
ChaseRensberger <chasehrensberger@gmail.com>
2024-12-22 19:52:26 -0500

    try to ignore hw reqs

--- Formula/check-the-score.rb
+++ Formula/check-the-score.rb
@@ -2,16 +2,8 @@ class CheckTheScore < Formula
   desc "CLI tool for checking sports scores"
   homepage "https://github.com/ChaseRensberger/check-the-score"
   version "0.1.0"
-
-  on_macos do
-    if Hardware::CPU.arm?
-      url "https://github.com/ChaseRensberger/check-the-score/releases/download/v0.1.0/check-the-score-arm64"
-      sha256 "eb753f053597365ccd3474dc002d570b8f931f33c0c60c9d57216a136d02c16d"
-    else
-      url "https://github.com/ChaseRensberger/check-the-score/releases/download/v0.1.0/check-the-score-amd64"
-      sha256 "eb753f053597365ccd3474dc002d570b8f931f33c0c60c9d57216a136d02c16d"
-    end
-  end
+  url "https://github.com/ChaseRensberger/check-the-score/releases/download/v0.1.0/check-the-score"
+  sha256 "eb753f053597365ccd3474dc002d570b8f931f33c0c60c9d57216a136d02c16d"
 
   def install
     bin.install "check-the-score"
