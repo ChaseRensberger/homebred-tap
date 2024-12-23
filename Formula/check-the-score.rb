11203908dff29b58705f912916867ebd8baa6337
ChaseRensberger <chasehrensberger@gmail.com>
2024-12-22 20:06:27 -0500

    update sha

--- Formula/check-the-score.rb
+++ Formula/check-the-score.rb
@@ -3,7 +3,7 @@ class CheckTheScore < Formula
   homepage "https://github.com/ChaseRensberger/check-the-score"
   version "0.1.0"
   url "https://github.com/ChaseRensberger/check-the-score/releases/download/v0.1.0/check-the-score"
-  sha256 "eb753f053597365ccd3474dc002d570b8f931f33c0c60c9d57216a136d02c16d"
+  sha256 "72cf66641900b8873f2ca38bb85cdf086bdea44bfd70694e5d33357f4c47d5bc"
 
   def install
     bin.install "check-the-score"
